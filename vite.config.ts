beb20feafc8e884eea9532ec1c85dd7669aec802
l3n641 <371522155@qq.com>
2026-05-27 09:39:15 +0800

    add npm chrome app

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,24 @@
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import vue from '@vitejs/plugin-vue'
+import {crx} from '@crxjs/vite-plugin'
+import manifest from './manifest.json'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+    plugins: [vue(), crx({manifest})],
+    server: {
+        port: 5173,
+        strictPort: true,
+        // 🔥 核心配置：允许跨域
+        cors: true,
+        headers: {
+            'Access-Control-Allow-Origin': '*',
+        },
+        // 如果你在使用特定的热更新配置，也可以尝试指定 hmr 客户端协议
+        hmr: {
+            clientPort: 5173,
+        },
+    },
 })
+
+
